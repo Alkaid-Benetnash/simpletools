a8422db34655620fae54b6c64d1c068b89500f43
Alkaid <zgf574564920@gmail.com>
2018-04-15 14:25:36 +0800

    add cli param explanation

--- bin/unix_ftpd.py
+++ bin/unix_ftpd.py
@@ -18,6 +18,23 @@ from pyftpdlib.servers import FTPServer
 
 def main():
     authorizer = DummyAuthorizer()
+    """
+    Read permissions:
+
+        "e" = change directory (CWD, CDUP commands)
+        "l" = list files (LIST, NLST, STAT, MLSD, MLST, SIZE commands)
+        "r" = retrieve file from the server (RETR command)
+    Write permissions:
+
+        "a" = append data to an existing file (APPE command)
+        "d" = delete file or directory (DELE, RMD commands)
+        "f" = rename file or directory (RNFR, RNTO commands)
+        "m" = create directory (MKD command)
+        "w" = store a file to the server (STOR, STOU commands)
+        "M" = change file mode / permission (SITE CHMOD command) New in 0.7.0
+        "T" = change file modification time (SITE MFMT command) New in 1.5.3
+    """
+    authorizer.add_user('admin', 'adminpassword', '.', perm='elrafmwMT')
     authorizer.add_anonymous('/mnt/Data_Dual')
     handler = FTPHandler
     handler.authorizer = authorizer
