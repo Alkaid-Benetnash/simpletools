a50d52a8351a54e798f28548ffed5b71928d290c
Alkaid <zgf574564920@gmail.com>
2019-11-04 10:39:42 -0500

    Add ucode.py and netconsole.sh

---
+++ netconsole.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+target=backup
+netconsole_path=/sys/kernel/config/netconsole/${target}
+dmesg -E
+dmesg -n 8 //set console log level to 8
+modprobe configfs
+modprobe netconsole
+mount none -t configfs /sys/kernel/config
+mkdir /sys/kernel/config/netconsole/${target}
+cd /sys/kernel/config/netconsole/${target}
+echo '202.38.70.210' > $netconsole_path/remote_ip
+echo '6555' > $netconsole_path/remote_port
+
