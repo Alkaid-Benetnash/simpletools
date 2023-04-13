c59346d704fd26ec403e43f54b340a4472458836
Alkaid <zgf574564920@gmail.com>
2023-04-13 02:49:26 -0400

    Remove outdated stuff

--- freeshell_blame_cpu.sh
+++
@@ -1,1 +0,0 @@
-for ((i=1;i<=7;++i)); do echo s$i; ssh s$i ps aux --sort=+pcpu | tail -n 10; done
--- freeshell_find_shadowsocks.sh
+++
@@ -1,1 +0,0 @@
-for ((i=1;i<=7;++i)); do echo s$i; ssh s$i ps aux | grep -e shadow -e vpnserver; done
