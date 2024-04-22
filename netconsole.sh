34f8d67c399a13740ace86d1f45e2b90d4f3fe8b
Alkaid <zgf574564920@gmail.com>
2024-04-21 21:26:01 -0400

    Update netconsole

--- netconsole.sh
+++ netconsole.sh
@@ -2,12 +2,11 @@
 target=backup
 netconsole_path=/sys/kernel/config/netconsole/${target}
 dmesg -E
-dmesg -n 8 //set console log level to 8
+dmesg -n 8 #set console log level to 8
 modprobe configfs
 modprobe netconsole
 mount none -t configfs /sys/kernel/config
 mkdir /sys/kernel/config/netconsole/${target}
 cd /sys/kernel/config/netconsole/${target}
-echo '202.38.70.210' > $netconsole_path/remote_ip
+echo '192.168.100.1' > $netconsole_path/remote_ip
 echo '6555' > $netconsole_path/remote_port
-
